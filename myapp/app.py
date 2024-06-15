6d4c2a7e78e00936a25c22577338ac16beaf8f82
Adam <adam@gatherer.tech>
2024-06-15 12:24:47 +0100

    Added Python code

--- myapp/app.py
+++ myapp/app.py
@@ -1,4 +1,15 @@
 from pyfiglet import Figlet
-import Cowsay
+import cowsay
+import argparse
 
-print('Hello World')
+def main(name: str):
+    message = "Hello, my name is " + name
+    cowsay.cow(message)
+    f = Figlet(font='digital')
+    print(f.renderText(name))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Processes arguments.')
+    parser.add_argument('--name', type=str, default='Betsy', help='Name your cow')
+    args = parser.parse_args()
+    main(args.name)
